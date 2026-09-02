d14264359c812bda77dc617448a9836ef8d233d6
SentinelShadow <vineethkumar.v6@gmail.com>
2026-09-02 15:31:21 +0530

    section 7 - mysql code changes

---
+++ section7/accounts/src/main/java/com/microservice/practice/accounts/dto/AccountsDto.java
@@ -0,0 +1,33 @@
+package com.microservice.practice.accounts.dto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
+import lombok.Data;
+
+@Data
+@Schema(
+        name = "Accounts",
+        description = "Schema to hold Account information"
+)
+public class AccountsDto {
+
+    @Schema(
+            description = "Account number of the customer", example = "1234567890"
+    )
+    @NotEmpty(message = "AccountNumber can not be a null or empty")
+    @Pattern(regexp="(^$|[0-9]{10})",message = "AccountNumber must be 10 digits")
+    private Long accountNumber;
+
+    @Schema(
+            description = "Type of the account", example = "Savings"
+    )
+    @NotEmpty(message = "AccountType can not be a null or empty")
+    private String accountType;
+
+    @Schema(
+            description = "Address of the branch", example = "123 Main Street"
+    )
+    @NotEmpty(message = "BranchAddress can not be a null or empty")
+    private String branchAddress;
+}
